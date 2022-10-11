1201d377ed604f4a2381d2716bb1220225d57425
MrTimeout <estonoesmiputocorreo@gmail.com>
2022-10-11 15:08:17 +0200

    Adding some features

--- opengl/start/README.md
+++ opengl/start/README.md
@@ -5,3 +5,4 @@
 - [Example code](https://www.glfw.org/documentation.html)
 - [How to compile GLFW](https://www.glfw.org/docs/latest/compile.html)
 - [Glad generator](https://www.glfw.org/documentation.html)
+- [First triangle](http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/)
--- opengl/start/main.cpp
+++ opengl/start/main.cpp
@@ -6,6 +6,11 @@
 #define HEIGHT 480
 
 int main() {
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old opengl
+
   GLFWwindow* window;
 
   if (!glfwInit()) return -1;
@@ -18,9 +23,10 @@ int main() {
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
   /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window))
+  while (glfwGetKey(window, GLFW_KEY_SPACE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0)
   {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
